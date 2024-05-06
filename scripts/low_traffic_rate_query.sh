0aec26f1451f9c184b318b570c1cd8de8f190bd2
ZM_L <614292478@qq.com>
2024-05-06 12:57:31 +0100

    Adding a script for testing low traffic rate query

---
+++ scripts/low_traffic_rate_query.sh
@@ -0,0 +1,5 @@
+./../build/valhalla_service ../valhalla.json route '{"locations":[{"lat":41.7881185,"lon":-87.7009504},{"lat":41.7899513,"lon":-87.6997816}],"costing":"pedestrian","costing_options":{"pedestrian":{"use_lit":0,"less_crime_rate":false}},"directions_options":{"units":"kilometers"},"date_time":{"type":1,"value":"2024-05-06T10:04"}}' | jq '.'
+
+# Somewhere in Chicago
+# 41.7881185, -87.7009504
+# 41.7899513, -87.6997816
