0610d9ae6dde7214bad3f3575b24eb1ed7c53c26
ZM_L <614292478@qq.com>
2024-05-15 15:57:04 +0100

    Adding a script which is used when valhalla is set up as a server

--- scripts/low_traffic_rate_query.sh
+++ scripts/low_traffic_rate_query.sh
@@ -1,5 +1,8 @@
 ./../build/valhalla_service ../valhalla.json route '{"locations":[{"lat":51.50405,"lon":-0.19222},{"lat":51.5092,"lon":-0.1954}],"costing":"pedestrian","costing_options":{"pedestrian":{"use_lit":0,"less_crime_rate":false,"low_traffic_rate":true}},"directions_options":{"units":"kilometers"},"date_time":{"type":1,"value":"2024-05-06T18:00"}}' | jq '.'
 
+# Used when set up the server
+curl http://localhost:8002/route -d '{"locations":[{"lat":51.50405,"lon":-0.19222},{"lat":51.5092,"lon":-0.1954}],"costing":"pedestrian","costing_options":{"pedestrian":{"use_lit":0,"less_crime_rate":false,"low_traffic_rate":true}},"directions_options":{"units":"kilometers"},"date_time":{"type":1,"value":"2024-05-06T18:00"}}' | jq '.'
+
 # Passing Kensington Church Street
 # 51.50405,-0.19222
 # 51.5092,-0.1954
